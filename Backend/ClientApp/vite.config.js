f288f7e60556f87ca49fe2fdd8d8ef6fb8177394
otravlen <karmawayso@gmail.com>
2025-07-31 01:33:12 +0300

    Solution created

---
+++ Backend/ClientApp/vite.config.js
@@ -0,0 +1,34 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import checker from 'vite-plugin-checker';
+
+export default defineConfig({
+  plugins: [
+    react()    
+  ],
+  server: {
+    port: 3000,
+    strictPort: true, 
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  },
+  build: {
+    outDir: '../wwwroot',
+    emptyOutDir: true,
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].[hash].js`,
+        chunkFileNames: `assets/[name].[hash].js`,
+        assetFileNames: `assets/[name].[hash].[ext]`
+      }
+    }
+  },
+  base: './' 
+})
